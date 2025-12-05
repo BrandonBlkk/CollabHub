8ce5609bd29e2eec95c4c750cc765fd56736b3c4
Kyaw <kyawzayartun0527@gmail.com>
2025-12-05 23:30:27 +0630

    feat: add freelancer profile page

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,25 @@ Route::get('/', function () {
     return view('startup');
 })->name('startup');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
-    Route::get('/find-freelancers', [FindFreelancersController::class, 'index'])->name('find-freelancers');
+    })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Client-Only Routes
+    Route::middleware('user.type:client')->group(function () {
+        Route::get('/find-freelancers', [FindFreelancersController::class, 'index'])
+            ->name('find-freelancers');
+        Route::get('/freelancer/{id}', [FindFreelancersController::class, 'freelancerProfile'])
+            ->name('freelancer-profile');
+    });
+
+    //  Freelancer-Only Routes
+    Route::middleware('user.type:freelancer')->group(function () {});
 });
 
 require __DIR__ . '/auth.php';
